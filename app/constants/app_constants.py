80ce7532d57cbd890335d971e6e2d34a5a258d90
Simran KC <074bct541.simran@pcampus.edu.np>
2023-02-28 10:41:56 +0545

    Fixed merge conflict issues

--- app/constants/app_constants.py
+++
@@ -1,7 +0,0 @@
-# Application constants that are repeated throughout the app
-
-state = "state"
-authorization = "Authorization"
-refresh_token = "RefreshToken"
-responder = "RESPONDER"
-creator = "CREATOR"
--- app/models/schemas/allowed_origins.py
+++
@@ -1,8 +0,0 @@
-from app.models.common.mongo_document import MongoDocument
-
-
-class AllowedOriginsDocument(MongoDocument):
-    origin: str
-
-    class Collection:
-        name = "allowedOrigins"
