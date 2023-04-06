cc958e896b5a8d86e2000be635b879dc9deed811
Simran KC <074bct541.simran@pcampus.edu.np>
2023-04-06 19:31:46 +0545

    Added env file for new configuration

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e # Exit with nonzero exit code if anything fails
 
-prisma migrate deploy 
-prisma generate
+exec python3 -m prisma migrate deploy 
+exec python3 -m prisma generate
 exec python3 -m uvicorn main:app --host 0.0.0.0 --port 8080
