6e87288c8551568801ed1d900d986c38340880c7
Simran K.C <simran@office.sireto.io>
2023-04-13 14:14:02 +0545

    client ip fix and method ws capitzalize in logs

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 set -e # Exit with nonzero exit code if anything fails
 
 prisma migrate deploy 
-exec python3 -m uvicorn main:app --host 0.0.0.0 --port 8080
+exec python3 -m uvicorn --proxy-headers --forwarded-allow-ips="*"  main:app --host 0.0.0.0 --port 8080
