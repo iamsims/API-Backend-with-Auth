87224c2e497c8c9b0ec831552577ef7f2d61c5da
Simran KC <074bct541.simran@pcampus.edu.np>
2023-04-24 10:24:25 +0545

    WIP save api key in binary

--- app/auth/api_key.py
+++ app/auth/api_key.py
@@ -7,7 +7,6 @@ def generate_api_key():
     try:
         random_bytes = secrets.token_bytes(48)
         api_key = base64.b64encode(random_bytes).decode('utf-8')
-        #remove paddinng characters
         api_key = api_key.replace("=","")
         api_key = api_key.replace("+","")
         api_key = api_key.replace("/","")        
@@ -19,4 +18,23 @@ def generate_api_key():
         detail="Exception in generating api key"
         )
     
+
+def tobinary(api_key):
+    try:
+        return base64.b64decode(api_key)
+    except:
+        raise HTTPException(
+        status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        detail="Exception in converting api key to binary"
+        )
+
+
+def tostring(api_key):
+    try:
+        return base64.b64encode(api_key)
+    except:
+        raise HTTPException(
+        status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        detail="Exception in converting api key to string"
+        )
 
