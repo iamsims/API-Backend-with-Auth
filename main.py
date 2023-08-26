23bf6281b8ea26205737b99756d5603034922edf
Simran KC <074bct541.simran@pcampus.edu.np>
2023-08-26 09:07:13 +0545

    Remove unnecessary imports

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import sys, os
 from fastapi import FastAPI
 from app.api.auth import router as users_router
 from app.api.api_key import router as api_router
-from app.api.snippet import router as snippet_router
 
 from starlette.middleware.sessions import SessionMiddleware
 from decouple import config
@@ -39,7 +38,6 @@ app.add_middleware(
 
 app.include_router(users_router, prefix= "/auth")
 app.include_router(api_router, prefix = "/api/v1")
-app.include_router(snippet_router, prefix = "/snippet")
 
 @app.on_event('startup')
 async def startup_event():
@@ -57,6 +55,7 @@ async def startup_event():
 @app.on_event('shutdown')
 async def shutdown_event():
     await prisma.disconnect()
+
 
 
 
